4148aeddc206e9b504780c78d0fb561101611f7b
Anthony-Phimmasone <31628907+Anthony-Phimmasone@users.noreply.github.com>
2019-03-12 14:42:38 -0400

    Add files via upload
    User can type in CSV filename of file they want to use.
    If the filename is not found it will ask the user to type name again.
    If the filename is found it will print the dataframe.
    It will automatically write to a file, but currently only outputs to a blank csv.

--- GroupMaker.py
+++ GroupMaker.py
@@ -5,15 +5,31 @@ Created on Sun Mar 10 09:26:05 2019
 @author: phimm_000
 """
 
+import os
+
 #Group Maker
 #Anthony Phimmasone
-#
-#
-#
+#Denzel Saraka
+#Igor Asipenka
+#James Donahue
 
 import pandas as pd
 
-df = pd.read_csv('GroupMakerSample.csv')
+#Ask the user to enter in a CSV filename
+filename = input("Enter a filename: ")
+
+#While the file does not exist
+#Ask the user to re-enter filename
+#The path of the file
+while not os.path.exists('./' + filename):
+    print("File does not exist or incorrect spelling")
+    filename = input("Enter a filename: ")
+
+#Print the CSV file
+df = pd.read_csv(filename)
 print(df)
 
-# column = ['Column 1', 'Column 2', 'Column 3, 'Column 4', 'Column 5']
+#Write to the CSV
+file = open('GroupMakerOutput.csv', "w")
+#df.to_csv(index=False)
+print("Successfully wrote to output file!")
