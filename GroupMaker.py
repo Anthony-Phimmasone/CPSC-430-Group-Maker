fb4de174387e850ee69bca5790ebb94be6f62f5c
Anthony-Phimmasone <31628907+Anthony-Phimmasone@users.noreply.github.com>
2019-03-12 13:22:52 -0400

    Add files via upload
    One line to read a CSV and a sample CSV file.

---
+++ GroupMaker.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 10 09:26:05 2019
+
+@author: phimm_000
+"""
+
+#Group Maker
+#Anthony Phimmasone
+#
+#
+#
+
+import pandas as pd
+
+df = pd.read_csv('GroupMakerSample.csv')
+print(df)
+
+# column = ['Column 1', 'Column 2', 'Column 3, 'Column 4', 'Column 5']
